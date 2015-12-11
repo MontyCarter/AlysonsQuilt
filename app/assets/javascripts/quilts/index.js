f154bb27430e5f0a688a435eed9059aa80095a55
Charlie Jacobsen <charlie.jacobsen@utah.edu>
2015-12-10 22:15:48 -0700

    Filler squares set up, laid out. Now for regular squares...

--- app/assets/javascripts/quilts/index.js
+++ app/assets/javascripts/quilts/index.js
@@ -1,7 +1,4 @@
 
-    // get filler squares
-    //   -- build object with:
-    //      -- img src for square
     // process regular squares
     // create divs with inner images
     //   -- thumbnail
@@ -16,20 +13,77 @@ function quilts_index() {
 
     NUM_FILLER_SQUARES = 10;
 
+    function init_masonry() {
+
+	$('.grid').imagesLoaded().done( function(instance) {
+
+	    $grid = $('.grid')
+
+	    // Masonry seems limited in that you have to display
+	    // the images for it to lay things out.
+	    $grid.show();
+
+	    // Do the layout
+	    $grid.masonry({
+		// square containers
+		itemSelector: '.grid-item',
+		// for sizing:
+		columnWidth: '.grid-sizer',
+		// use percentages for grid column widths (see css)
+		percentPosition: true
+	    });
+
+	    $(window).resize(function() {
+
+		$grid.masonry();
+
+		// todo: resize images?
+
+	    });
+
+	});
+    }
+
+    function build_quilt(squares) {
+
+	var html = '<div class="grid" style="display:none;">';
+	html +=        '<div class="grid-sizer"></div>';
+	
+	for (var i = 0; i < squares.length; i++)
+	    html += squares[i].build_square_div();
+
+	html += '</div>';
+	
+	$('#quilt-main').html(html);
+
+    }
+
+    function build_filler_square_div() {
+	var str = '<div class="grid-item">' +
+	    '<img src="' + this.media_medium_url + '" alt="pattern" />' +
+	    '</div>';
+	return str;
+    }
+
     function build_filler_squares(num) {
 
-	squares = [];
+	var squares = [];
 
-	for (i = 0; i < num; i++) {
+	for (var i = 0; i < num; i++) {
 	    
 	    // Choose random pattern index
-	    idx = Math.floor((Math.random() * NUM_FILLER_SQUARES) + 1); 
+	    var idx = Math.floor((Math.random() * NUM_FILLER_SQUARES)); 
 
 	    // Build new square
 	    squares.push(
 		{
-		    type: 'filler',
-		    url: '/patterns/medium/' + idx.toString() + '.png'
+		    media_small_url: '/patterns/small/' + 
+			idx.toString() + '.png',
+		    media_medium_url: '/patterns/medium/' + 
+			idx.toString() + '.png',
+		    media_large_url: '/patterns/large/' + 
+			idx.toString() + '.png',
+		    build_square_div: build_filler_square_div
 		});
 	}
 
@@ -38,20 +92,24 @@ function quilts_index() {
 
     function setup_quilt(data, textStatus, jqXHR) {
 
-	real_squares = data;
+	var real_squares = data;
 	
-	// Add type = 'real' to real squares
-	for (i = 0; i < real_squares.length; i++)
-	    real_squares[i][type] = 'real';
+	// Add fields to real squares
+	for (var i = 0; i < real_squares.length; i++) {
+	    real_squares[i][build_square_div] = build_real_square_div;
+	}
 
 	// Build filler squares (multiples of 5, at least 5)
-	num_fillers = Math.ceil((real_squares.length + 5) / 5);
-	filler_squares = build_filler_squares(num_fillers);
+	var num_fillers = 20 * Math.ceil((real_squares.length + 5) / 5);
+	var filler_squares = build_filler_squares(num_fillers);
 
-	
+	// Build quilt with all squares
+	build_quilt(real_squares.concat(filler_squares));
 
-	$('#quilt-main').html('<p>f' + data.length + 'e</p>');
+	// Initialize masonry library
+	init_masonry();
 
+	// Done!
     }
 
     function handle_failure(jqXHR, textStatus, errorThrown) {
@@ -63,25 +121,6 @@ function quilts_index() {
     $.get('/squares')
     .done( setup_quilt )
     .fail( handle_failure );
-
-    // $('.grid').imagesLoaded().done( function(instance) {
-
-    // 	$('.grid').masonry({
-
-    // 	    itemSelector: '.grid-item',
-    // 	    columnWidth: '.grid-sizer',
-    // 	    percentPosition: true
-
-    // 	})
-
-    // 	$(window).resize(function() {
-
-    // 	    $('.grid').masonry();
-
-    // 	});
-
-    // });
-
 
 }
 
